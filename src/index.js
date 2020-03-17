c99e22f9270db4667d2bcaead46f1b0d4f3436f5
Derevygo Evgeniya <evgeniya.nice@yandex.by>
2020-03-17 16:26:02 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if(!Array.isArray(array) || array.length==0 ){
+    return 0;}
+   let minEl=array[0];
+   array.forEach(element => { 
+     console.log(element);
+     if(element<minEl){
+      //console.log(minEl);
+      minEl=element;
+   }    
+   });
+       return minEl; 
+  }
 
 exports.max = function max (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length==0 ){
+    return 0;
+  }else{
+  let maxEl=array[0];
+  array.forEach(element => { 
+    if(element>maxEl){
+     maxEl=element;
+  }    
+  });
+  return maxEl;
+ }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!Array.isArray(array) || array.length==0 ){
+    return 0;}
+  let sum=0;
+  array.forEach(element => sum+=element)
+  return sum/(array.length);
+
+  
 }
